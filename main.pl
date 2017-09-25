7230c7375396938eea4d35919191aa14f5eb0f1d
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-09-25 11:05:35 -0300

    Minor fix to the switch structure

--- main.pl
+++ main.pl
@@ -1,15 +1,24 @@
 % Model for the game structure
+play(no) :- write('-- GAME OVER --'), nl, !.
 
-play(NO) :- write('-- GAME OVER --'), nl, !.
-
-play :- write('-- Welcome to a shitty Batman deduction game'), nl,
+play :-
+        write('-- Welcome to a shitty Batman deduction game'), nl,
         write('-- Your objective is to track down Black Mask and discover...'), nl,
         write('-- 1 - Choice 1 ...'), nl,
         write('-- 2 - Choice 2 ...'), nl,
-        read(choice),
-        step1(choice).
 
-play(YES) :- play.
+        read(Choice),
+        stage1(Choice).
+
+play(yes) :- play.
 
 % Next step and choice from previous one
-step1(1) :- write('-- Something --'), nl, !.
+stage1(1) :-
+        write('-- Something 1 --'), nl,
+        read(Continue),
+        play(Continue).
+
+stage1(2) :-
+        write('-- Something 2 --'), nl,
+        read(Continue),
+        play(Continue).
