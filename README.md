4c6f65ea54dbc8675fbbbd182a42ce08ab3a215c
CaioNunes <caiofelipe147@gmail.com>
2017-09-26 11:23:12 -0300

    Create basic structure to game

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # T2_PROLOG
 Repositório para implementação do segundo trabalho da disciplina de Paradigmas de Programação
+
+1. Descrição
+
+O jogador assume o papel do Batman e precisa descobrir o vilão responsável pelo crime, através de raciocínio detutivo em cima de pistas descobertas dentro do jogo !
--- main.pl
+++ main.pl
@@ -1,24 +1,42 @@
 % Model for the game structure
-play(no) :- write('-- GAME OVER --'), nl, !.
 
+play(no) :- write('-- GAME OVER --'), nl, !.
 play :-
         write('-- Welcome to a shitty Batman deduction game'), nl,
         write('-- Your objective is to track down Black Mask and discover...'), nl,
-        write('-- 1 - Choice 1 ...'), nl,
-        write('-- 2 - Choice 2 ...'), nl,
+        write('-- 1 - Case 1: Black Bomb ...'), nl,
+        write('-- 2 - Case 2: Red Apple ...'), nl,
 
         read(Choice),
-        stage1(Choice).
+        caso(Choice).
 
 play(yes) :- play.
 
 % Next step and choice from previous one
-stage1(1) :-
-        write('-- Something 1 --'), nl,
-        read(Continue),
-        play(Continue).
+caso(1) :-
+        write('-- CASE DESCRIPTION --'), nl,
+        write('-- SELECT THE COMPUTER OPTIONS --'), nl,
+        write('-- 1 - PEOPLE INFORMATION --'), nl,
+        write('-- 2 - PLACES INFORMATION --'), nl,
+        write('-- 3 - WEAPONS INFORMATION --'), nl,
 
-stage1(2) :-
-        write('-- Something 2 --'), nl,
-        read(Continue),
-        play(Continue).
+        % Escolhe qual tipo de informação ele deseja = Pessoas, Locais, Armamento.
+        read(Options),
+        Caso = 1,
+        computer_options(Options, Caso).
+
+computer_options(1, Caso) :-
+        write('-- PEOPLE INFORMATION --'), nl,
+        write('-- SELECT THE PERSON --'), nl,
+        write('-- 1 - HERA VENENOSA --'), nl,
+        write('-- 2 - CHARADA --'), nl,
+        write('-- 3 - CORINGA --'), nl,
+
+        % Escolhe qual pessoa ele deseja ter informações
+        read(Person),
+        see_person_information(Person, Caso).
+
+see_person_information(1, Caso) :-
+        write('-- 1 - HERA VENENOSA INFORMATION --'), nl,
+        write('-- HERA INFORMATION --'), nl,
+        caso(Caso).
