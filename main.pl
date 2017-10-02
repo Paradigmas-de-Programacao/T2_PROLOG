09b03ab3819805d16a8731c24576f22782316ee9
IagoCarvalho <IagoVC_2012@hotmail.com>
2017-10-02 02:13:37 -0300

    Adding final clues method

--- main.pl
+++ main.pl
@@ -20,7 +20,7 @@ play :-
         file_to_list, nl,
         write('-- Welcome to a Batman deduction game --'), nl,
         write('-- Your objective is solve all archived cases of police --'), nl,
-        write('-- 1 - Case 2: Red Apple --'), nl,
+        write('-- 1 - Case 1: Red Apple --'), nl,
 
         read(Choice),
         caso(Choice).
@@ -30,7 +30,7 @@ play(yes) :- play.
 % Next step and choice from previous one
 caso(1) :-
         cls,
-        write('-- A QUINTA ESPOSA DO COMISSÁRIO GORDON FOI ENVENENADA POR UMA MAÇÃ VERMELHA ENCONTRADA PRÓXIMA A JANELA DE SUA CASA. FOI DITO QUE DURANTE A NOITE, UMA SOMBRA FOI VISTA ENTRANDO NA CASA, E SAINDO RAPIDAMENTE. QUANDO A SOMBRA ENTROU ATRÁS DE UMA ÁRVORE, DESAPARECEU. --'), nl,
+        write('-- A ESPOSA DO COMISSÁRIO GORDON FOI ENVENENADA POR UMA MAÇÃ VERMELHA ENCONTRADA PRÓXIMA A JANELA DE SUA CASA. FOI DITO QUE DURANTE A NOITE, UMA SOMBRA FOI VISTA ENTRANDO NA CASA, E SAINDO RAPIDAMENTE. QUANDO A SOMBRA ENTROU ATRÁS DE UMA ÁRVORE, DESAPARECEU. --'), nl,
         write('-- SELECT THE COMPUTER OPTIONS --'), nl,
         write('-- 1 - PEOPLE INFORMATION --'), nl,
         write('-- 2 - PLACES INFORMATION --'), nl,
@@ -89,45 +89,103 @@ clearBase1(X):- retract(X).
 clearBase1(X).
 
 
-culprit(joker) :- clue(wears_purple),
+culprit(crime_culprit) :-
+                  clue(is_a_human),
+                  clue(is_a_monster),
+                  clue(is_a_scientist),
+                  clue(is_a_man),
+                  clue(is_a_woman),
+                  clue(wears_purple),
+                  clue(wears_black),
+                  clue(wears_green),
+                  clue(wears_armor),
+                  clue(has_no_ally),
+                  clue(has_ally),
                   clue(uses_gun),
-                  clue(laughs),
-                  clue(has_one_ally),
                   solve.
 
-clue(wears_purple) :-
-        write('Does the suspect wears purple? (y/n)'),nl,
-        read(AnswerPurple),
-        affirmative_clue(AnswePurple, wears_purple).
+clue(is_a_human) :-
+        write('Is the culprit probably a human? (y/n)'),nl,
+        read(AnswerHuman),
+        affirmative_clue(AnswerHuman, is_a_human).
 
+clue(is_a_monster) :-
+        write('Is the culprit probably a monster? (y/n)'),nl,
+        read(AnswerMonster),
+        affirmative_clue(AnswerHuman, is_a_monster).
+
+clue(is_a_scientist) :-
+        write('Is the culprit probably a scientist (y/n)'),nl,
+        read(AnswerScientist),
+        affirmative_clue(AnswerScientist, is_a_scientist).
+
+clue(is_a_man) :-
+        write('Is the culprit probably a man (y/n)'),nl,
+        read(AnswerMan),
+        affirmative_clue(AnswerMan, is_a_man).
+
+clue(is_a_woman) :-
+        write('Is the culprit probably a woman (y/n)'),nl,
+        read(AnswerWoman),
+        affirmative_clue(AnswerWoman, is_a_woman).
+
+clue(wears_purple) :-
+        write('Did the suspect wears purple? (y/n)'),nl,
+        read(AnswerPurple),
+        affirmative_clue(AnswerPurple, wears_purple).
+
+clue(wears_black) :-
+        write('Did the suspect wears black? (y/n)'),nl,
+        read(AnswerBlack),
+        affirmative_clue(Answerblack, wears_black).
+
+clue(wears_green) :-
+        write('Did the suspect wears green? (y/n)'),nl,
+        read(AnswerGreen),
+        affirmative_clue(AnswerGreen, wears_green).
+
+clue(wears_armor) :-
+        write('Did the suspect wears a armor? (y/n)'),nl,
+        read(AnswerArmor),
+        affirmative_clue(AnswerArmor, wears_armor).
+
+clue(has_no_ally):-
+        write('Did the suspect had no ally? (y/n)'),nl,
+        read(AnswerNoAlly),
+        affirmative_clue(AnswerNoAlly, has_no_ally).
+
+clue(has_ally):-
+        write('Did the suspect had an ally? (y/n)'),nl,
+        read(AnswerAlly),
+        affirmative_clue(AnswerAlly, has_ally).
 
 clue(uses_gun):-
-        write('Does the suspect uses a gun? (y/n)'),nl,
+        write('Did the suspect used a gun? (y/n)'),nl,
         read(AnswerGun),
         affirmative_clue(AnswerGun, uses_gun).
-
-
-clue(laughs):-
-        write('Does the suspect laughs at the crime scene? (y/n)'),nl,
-        read(AnswerLaughs), affirmative_clue(AnswerLaughs, laughs).
-
-
-clue(has_one_ally):-
-        write('Does the suspect has only one ally? (y/n)'),nl,
-        read(AnswerOneAlly),
-        affirmative_clue(AnswerOneAlly, has_one_ally).
 
 affirmative_clue('y', Clue) :- asserta(affirmative(Clue)).
 affirmative_clue('n', Clue).
 
 solve :-
-        analysis,
-        write('The culprit data matches the Joker!'), nl, !.
+        analysis_joker,
+        write('The culprit data matches the Joker!'), nl,
+        write('But he wasnt the culprit of any of these cases!'), nl, !.
 
-solve :- write('The culprit was not the Joker !'), nl, !.
+solve :-
+        analysis_catwoman,
+        write('The culprit data matches Catwoman!'), nl, !.
 
-analysis :- (affirmative(wears_purple), affirmative(uses_gun), affirmative(laughs), affirmative(has_one_ally)).
+solve :-
+        analysis_poison_ivy,
+        write('The culprit data matches Poison Ivy!'), nl,
+        write('She was the culprit of case 1!'), nl, !.
 
+solve :- write('The culprit data doesnt match any villain in the database !'), nl, !.
+
+analysis_joker :- (affirmative(wears_purple), affirmative(uses_gun), affirmative(has_ally)).
+analysis_catwoman :- (affirmative(wears_black), affirmative(has_no_ally)).
+analysis_poison_ivy :- (affirmative(is_a_woman), affirmative(wears_green), affirmative(has_ally), affirmative(is_a_scientist)).
 
 see_person_information(Person, Caso) :-
         cls,
